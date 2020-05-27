b386f13724c430ee9b0613dcba2ab73aaa51e719
MacInLife <marie.ange.coco@gmail.com>
2020-05-27 14:08:00 +0200

    route web welcome laravel

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 //Liste les produits de la base  de données    
-Route::get('products' , 'ProductController@index')->name('index');
+Route::get('products', 'ProductController@index')->name('index');
 //Renvoi les produits et leurs quantités présent dans le panier
-Route::get('cart' , 'CartController@index')->name('index');
+Route::get('cart', 'CartController@index')->name('index');
 //Ajoute un produit dans le panier
-Route::post('cart' , 'CartController@store');
+Route::post('cart', 'CartController@store');
 //Vide le panier (get)
-Route::delete('cart' , 'CartController@destroy');
+Route::delete('cart', 'CartController@destroy');
 //Supprime un produit du panier  (get)
-Route::delete('cart/{productId}' , 'CartController@delete');
+Route::delete('cart/{productId}', 'CartController@delete');
